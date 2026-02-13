161af9520f9cf373652ebe374e9919cc37bf4ff6
Chanakya.Arora <Chanakya.Arora@in.ey.com>
2026-02-13 14:10:24 +0530

    Initial commit: Coming soon page with countdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dry-fruits-store';
+  title = 'NYN-Nutrition You Need';
+   launchDate: Date = new Date('2026-04-01');
+
+  get countdown() {
+    const now = new Date().getTime();
+    const distance = this.launchDate.getTime() - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds };
+  }
 }
